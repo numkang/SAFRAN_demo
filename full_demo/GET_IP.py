d4a55373882d7f83116e8d0c78695ded96c5f55e
numkang <thanakornkhamvilai@gmail.com>
2018-06-20 21:23:47 +0000

    full demo

--- full_demo/GET_IP.py
+++ full_demo/GET_IP.py
@@ -1,19 +1,15 @@
-import socket
+import subprocess
 
-def get_Host_name_IP():
-	host_name = socket.gethostname()
-	host_ip = socket.gethostbyname(host_name)
+RPI_ID = -1
 
-	if(host_ip[-2] != '.'):
-		host_id = host_ip[-2:]
-	else:
-		host_id = host_ip[-1]
+def get_IP():
+    global RPI_ID
+    ips = subprocess.check_output(['hostname', '--all-ip-addresses'])
+    ind = ips.index(" ")
+    if(ips[ind-2] != "."):
+       RPI_ID = int(ips[ind-2:ind-1]) - 2
+    else:
+       RPI_ID = int(ips[ind-1]) - 2       
 
-	print("Hostname : " + host_name)
-	print("IP : " + host_ip)
-	print(host_id)
-
-	return int(host_id) - 2
-
-RPI_ID = get_Host_name_IP()
+get_IP()
 print(RPI_ID)
