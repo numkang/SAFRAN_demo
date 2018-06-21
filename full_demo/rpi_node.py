f7a399a3caca32bc0e0ef75d33f66b564329cdb6
numkang <thanakornkhamvilai@gmail.com>
2018-06-21 17:52:13 +0000

    full demo

--- full_demo/rpi_node.py
+++ full_demo/rpi_node.py
@@ -8,6 +8,7 @@ import sys
 
 RPI_ID = -1
 app = np.array(['GHOST_LED.py', 'RED_LED.py', 'GREEN_LED.py', 'BLUE_LED.py', 'RGB_GPIO.py', 'OFF_LED.py'])
+p_app_open = -1
 app_open = -1
 
 switch_button = 40
@@ -16,6 +17,7 @@ GPIO.setup(switch_button, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 input_state = GPIO.input(switch_button)
 
 is_alive = '1'
+is_killed = False
 is_exit = 0
 proc = subprocess.Popen(['python', "empty.py"])
 
@@ -48,17 +50,15 @@ def on_message_func(client, userdata, message):
     global app
     global app_open
     global RPI_ID
-    print("message topic: ", message.topic)
+    #print("message topic: ", message.topic)
     print("message received: ", message.payload)
     # print("message qos: ", message.qos)
     # print("message retain flag: ", message.retain)
-    msg = int(message.payload[RPI_ID])
+    msg = int(message.payload[RPI_ID - 1])
     if(msg >= 0 and msg < len(app)):
         app_open = msg
     else:
         print("Invalid app open")
-
-    app_open = 0
 
 def on_log_func(client, userdata, level, string):
     pass
@@ -100,33 +100,27 @@ def call_func(threadName):
     global app_open
     global proc
     global is_alive
-    is_killed = False
-    p_app_open = -1
+    global is_killed
+    global p_app_open
+    #print(is_alive, is_killed)
 
     if(is_alive == '0' and is_killed == False):
-        #proc.kill()
-        #proc.wait()
-##            try:
-##                proc.kill()
-##                proc.wait()
-##            except:
-##                pass
-        proc = subprocess.Popen(['python', app[-1]]) #app to clean up the running app
-        #time.sleep(0.2)
         is_killed = True
+        print("OFF")
+        p_app_open = -1
+        proc.kill()
+        proc.wait()
+        proc = subprocess.Popen(['python', app[-1]]) #app to clean up the running app
     elif(is_alive == '1'):
+        #print("alive")
+        is_killed = False
         if(p_app_open != app_open):
+            print(p_app_open, app_open)
             p_app_open = app_open
-            #proc.kill()
-            #proc.wait()
-##              try:
-##                  proc.kill()
-##                  proc.wait()
-##              except:
-##                  pass
+            print(p_app_open, app_open)
+            proc.kill()
+            proc.wait()
             proc = subprocess.Popen(['python', app[app_open]]) #app to be run
-            #time.sleep(0.2)
-            is_killed = False
 
 # main loop
 def main():
@@ -140,8 +134,8 @@ def main():
 
         # Communication Setup
         broker_address = "192.168.0.2" # broker IP address
-        client_name = "rpi_node_" + str(RPI_ID + 1)
-        client_topic = "rpi/" + str(RPI_ID + 1)
+        client_name = "rpi_node_" + str(RPI_ID)
+        client_topic = "rpi/" + str(RPI_ID)
         client = mqtt.Client(client_name) # client's name
 
         # binding callback function
@@ -174,11 +168,8 @@ def main():
             pass
     except KeyboardInterrupt:
         is_exit = 1
-        print "Exit"        
-        try:
-            proc.kill()
-            proc.wait()
-        except:
-            pass
+        print "Exit"
+        proc.kill()
+        proc.wait()
         sys.exit(1)
 main()
