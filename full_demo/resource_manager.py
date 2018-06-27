7c400fa5ef6b086159187808a529312ebcfe82b9
Thanakorn Khamvilai <thanakornkhamvilai@gmail.com>
2018-06-26 20:33:00 -0400

    Update resource_manager.py

--- full_demo/resource_manager.py
+++ full_demo/resource_manager.py
@@ -122,7 +122,9 @@ def main():
 
             t1 = time.time()
             if (t1 - t0 > 1):
+		# arr = reconfiguration_func()
                 client.publish(topic = "resource_manager", payload = arr, qos = 0, retain = False)
+		node_status[:] = '0'
 		t0 = time.time()
 
 	    client.loop_start() # loop to enable callback functions	
