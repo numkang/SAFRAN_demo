08111318ed515337fbdbe709727f8dec9aa3ab36
numkang <thanakornkhamvilai@gmail.com>
2018-07-10 16:37:26 +0000

    full demo

---
+++ supermini/build/minisatp/CMakeFiles/minisatp.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Documents/supermini/minisatp/Main.cc" "/home/pi/Documents/supermini/build/minisatp/CMakeFiles/minisatp.dir/Main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "MINISAT_CONSTANTS_AS_MACROS"
+  "_FILE_OFFSET_BITS=64"
+  "__STDC_FORMAT_MACROS"
+  "__STDC_LIMIT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/arm-linux-gnueabihf"
+  "../minisat"
+  "../minisatp"
+  "../minisatp/ADTs"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/pi/Documents/supermini/build/minisatp/CMakeFiles/minisatp-lib-static.dir/DependInfo.cmake"
+  "/home/pi/Documents/supermini/build/minisat/CMakeFiles/minisat-lib-static.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
