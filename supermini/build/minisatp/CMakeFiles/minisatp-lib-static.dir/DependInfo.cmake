25b2268a9f52e93539a5eb040229fc939859a835
numkang <thanakornkhamvilai@gmail.com>
2018-07-10 16:33:55 +0000

    full demo

---
+++ supermini/build/minisatp/CMakeFiles/minisatp-lib-static.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/pi/Documents/supermini/minisatp/ADTs/FEnv.cc" "/home/pi/Documents/supermini/build/minisatp/CMakeFiles/minisatp-lib-static.dir/ADTs/FEnv.cc.o"
+  "/home/pi/Documents/supermini/minisatp/ADTs/File.cc" "/home/pi/Documents/supermini/build/minisatp/CMakeFiles/minisatp-lib-static.dir/ADTs/File.cc.o"
+  "/home/pi/Documents/supermini/minisatp/ADTs/Global.cc" "/home/pi/Documents/supermini/build/minisatp/CMakeFiles/minisatp-lib-static.dir/ADTs/Global.cc.o"
+  "/home/pi/Documents/supermini/minisatp/Debug.cc" "/home/pi/Documents/supermini/build/minisatp/CMakeFiles/minisatp-lib-static.dir/Debug.cc.o"
+  "/home/pi/Documents/supermini/minisatp/Hardware_adders.cc" "/home/pi/Documents/supermini/build/minisatp/CMakeFiles/minisatp-lib-static.dir/Hardware_adders.cc.o"
+  "/home/pi/Documents/supermini/minisatp/Hardware_clausify.cc" "/home/pi/Documents/supermini/build/minisatp/CMakeFiles/minisatp-lib-static.dir/Hardware_clausify.cc.o"
+  "/home/pi/Documents/supermini/minisatp/Hardware_sorters.cc" "/home/pi/Documents/supermini/build/minisatp/CMakeFiles/minisatp-lib-static.dir/Hardware_sorters.cc.o"
+  "/home/pi/Documents/supermini/minisatp/Main.cc" "/home/pi/Documents/supermini/build/minisatp/CMakeFiles/minisatp-lib-static.dir/Main.cc.o"
+  "/home/pi/Documents/supermini/minisatp/PbParser.cc" "/home/pi/Documents/supermini/build/minisatp/CMakeFiles/minisatp-lib-static.dir/PbParser.cc.o"
+  "/home/pi/Documents/supermini/minisatp/PbSolver.cc" "/home/pi/Documents/supermini/build/minisatp/CMakeFiles/minisatp-lib-static.dir/PbSolver.cc.o"
+  "/home/pi/Documents/supermini/minisatp/PbSolver_convert.cc" "/home/pi/Documents/supermini/build/minisatp/CMakeFiles/minisatp-lib-static.dir/PbSolver_convert.cc.o"
+  "/home/pi/Documents/supermini/minisatp/PbSolver_convertAdd.cc" "/home/pi/Documents/supermini/build/minisatp/CMakeFiles/minisatp-lib-static.dir/PbSolver_convertAdd.cc.o"
+  "/home/pi/Documents/supermini/minisatp/PbSolver_convertBdd.cc" "/home/pi/Documents/supermini/build/minisatp/CMakeFiles/minisatp-lib-static.dir/PbSolver_convertBdd.cc.o"
+  "/home/pi/Documents/supermini/minisatp/PbSolver_convertSort.cc" "/home/pi/Documents/supermini/build/minisatp/CMakeFiles/minisatp-lib-static.dir/PbSolver_convertSort.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "MINISAT_CONSTANTS_AS_MACROS"
+  "_FILE_OFFSET_BITS=64"
+  "__STDC_FORMAT_MACROS"
+  "__STDC_LIMIT_MACROS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/arm-linux-gnueabihf"
+  "../minisat"
+  "../minisatp"
+  "../minisatp/ADTs"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/pi/Documents/supermini/build/minisat/CMakeFiles/minisat-lib-static.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
